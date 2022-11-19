69abb3f418aefd443dbd20d7e48457d7c4425728
Robert Mollentze <robmllze@gmail.com>
2022-11-19 13:08:40 +1100

    updated GeneratedModel

--- lib/src/generated_model.dart
+++ lib/src/generated_model.dart
@@ -5,76 +5,56 @@
 // ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
 
 abstract class GeneratedModel {
-  //
-  //
-  //
-
+  /// Unique identifier fo this model.
   String? id;
 
-  //
-  //
-  //
+  /// Converts this model into a JSON object.
+  _Json toJson();
 
-  Map<String, dynamic> toJson();
+  /// Returns a copy of this model.
+  T copy<T extends GeneratedModel>(T other);
 
-  //
-  //
-  //
+  /// Constructs a new instance of type [T] from the JSON object [other].
+  T newFromJson<T extends GeneratedModel>(_Json other) {
+    return this.newEmpty().newOverrideJson(other);
+  }
 
-  /// Returns a new instance of type [T] with the fields in
-  /// [other] merged with/overriding the current fields.
-  T newWithJson<T extends GeneratedModel>(Map<String, dynamic> other);
+  /// Updates fields from the fields of [other].
+  void updateWithJson(_Json other);
 
-  //
-  //
-  //
+  /// Returns a new instance of type [T] with the fields in [other] merged
+  /// with/overriding the current fields.
+  T newOverrideJson<T extends GeneratedModel>(_Json other);
 
-  /// Returns a new instance of type [T] with the fields in
-  /// [other] merged with/overriding  the current fields.
-  T newWith<T extends GeneratedModel>(T other);
-
-  //
-  //
-  //
+  /// Returns a new instance of type [T] with the fields in [other] merged
+  /// with/overriding  the current fields.
+  T newOverride<T extends GeneratedModel>(T other);
 
   /// Returns a new instance of type [T] with empty fields.
   T newEmpty<T extends GeneratedModel>();
 
-  //
-  //
-  //
-
-  /// Updates fields from the fields of [other].
-  void updateWithJson(Map<String, dynamic> other);
-
-  //
-  //
-  //
-
   /// Updates fields from the fields of [other].
   void updateWith<T extends GeneratedModel>(T other);
 
-  // ---------------------------------------------------------------------------
-  // Utils for Firebase
-  // ---------------------------------------------------------------------------
-
-  dynamic refFirestore([String? path]) {
+  /// Returns a reference to this model on the server at the default path or at
+  /// [pathOverride] if provided.
+  dynamic refServer([String? pathOverride]) {
     throw UnimplementedError();
   }
 
-  //
-  //
-  //
-
-  Future<void> toFirestore({bool merge = true, String? path}) {
+  /// Writes this model to the server at the default path or at [pathOverride]
+  /// if provided.
+  Future<void> toServer({bool merge = true, String? pathOverride}) {
     throw UnimplementedError();
   }
 
-  //
-  //
-  //
-
-  Future<void> deleteFromFirestore({String? path}) {
+  /// Deletes this model from the server at the default path or at
+  /// [pathOverride] if provided.
+  Future<void> deleteFromServer({String? pathOverride}) {
     throw UnimplementedError();
   }
 }
+
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+
+typedef _Json = Map<String, dynamic>;
